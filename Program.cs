81e548cef319d240db5db324a8b48a5c3bf0afad
SnowLeo <alex.v.timofeev@gmail.com>
2026-06-28 18:19:18 +0500

    add logging; add check for config exists; add check for device folder exists

--- Program.cs
+++ Program.cs
@@ -1,29 +1,61 @@
 ﻿using System.Diagnostics;
 using System.Text.Json;
 using System.IO.Compression;
+using SubRealTeam.ConsoleUtility.Common.Logging;
 using SyncDeviceFolders;
 
+Logger.AddInstance(new ConsoleLogger());
+Directory.CreateDirectory("logs");
+Logger.AddInstance(new FileLogger(null, "logs"));
+
+if (!File.Exists("config.json"))
+{
+    Logger.Error("No config found. Please create 'config.json' file.");
+    var configSample = JsonSerializer.Serialize(new SyncFolderSettings
+    {
+        FolderSettings = new Dictionary<string, Dictionary<string, SyncFolderSetting>>
+        {
+            {
+                "Local folder name to sync", new Dictionary<string, SyncFolderSetting>
+                {
+                    {
+                        "device-name", new SyncFolderSetting
+                        {
+                            Path = "Path to sync folder on device",
+                            Process = "Process name: if need monitor running process",
+                            Filter = "Filter files: *.* by default"
+                        }
+                    }
+                }
+            }
+        }
+    }, new JsonSerializerOptions { WriteIndented = true, TypeInfoResolver = new AppJsonContext() });
+    Console.WriteLine("Example config:");
+    Console.WriteLine(configSample);
+    return;
+}
+    
 var configJson = File.ReadAllText("config.json");
 var config = JsonSerializer.Deserialize<SyncFolderSettings>(configJson,
     new JsonSerializerOptions { TypeInfoResolver = new AppJsonContext() });
 
 var machineName = Environment.MachineName;
-Console.WriteLine($"Machine name is: {machineName}");
+Logger.Info($"Machine name is: {machineName}");
 
 if (config == null)
 {
-    Console.WriteLine("No config found");
+    Logger.Error("No config found");
     return;
 }
 
 foreach (var folderSetting in config.FolderSettings)
 {
-    Console.WriteLine($"Config found: {folderSetting.Key}");
+    Logger.Info($"Config found: {folderSetting.Key}");
     var machineSettings = folderSetting.Value.Where(setting => setting.Key == machineName);
 
     foreach (var setting in machineSettings)
     {
-        Console.WriteLine($"Machine sync path is: {machineName}:{setting.Value.Path}");
+        Logger.Info($"Machine sync path is: {machineName}:{setting.Value.Path}");
 
         var syncFolder = Path.Combine(folderSetting.Key, "sync");
         var backupFolder = Path.Combine(folderSetting.Key, machineName, "backup");
@@ -42,7 +74,7 @@ return;
 
 async Task WaitForProcess(string processName, Action callback)
 {
-    Console.WriteLine($"Waiting for Process is starting: '{processName}' ...");
+    Logger.Info($"Waiting for Process is starting: '{processName}' ...");
     
     while (true)
     {
@@ -52,10 +84,10 @@ async Task WaitForProcess(string processName, Action callback)
             await Task.Delay(TimeSpan.FromMinutes(1));
         else
         {
-            Console.WriteLine($"Process found: '{processName}' (ID: {process.Id})");
-            Console.WriteLine($"Waiting for closing process '{processName}' ...");
+            Logger.Info($"Process found: '{processName}' (ID: {process.Id})");
+            Logger.Info($"Waiting for closing process '{processName}' ...");
             await process.WaitForExitAsync();
-            Console.WriteLine($"Process '{process.ProcessName}' closed! SYNC Saves ...");
+            Logger.Info($"Process '{process.ProcessName}' closed! SYNC Saves ...");
     
             callback();        
         }
@@ -65,24 +97,29 @@ async Task WaitForProcess(string processName, Action callback)
 
 void Sync(string syncFolder, string backupFolder, SyncFolderSetting setting)
 {
-    Console.WriteLine($"STARTING SYNC FOR {syncFolder}");
-    
+    Logger.Warn($"STARTING SYNC FOR {syncFolder}");
+
+    if (!Directory.Exists(setting.Path))
+    {
+        Logger.Error($"Device folder does not exist: {setting.Path}");
+        return;
+    }
     var files = Directory.GetFiles(setting.Path, setting.Filter, SearchOption.AllDirectories);
-    Console.WriteLine($"Found {files.Length} files");
+    Logger.Info($"Found {files.Length} files");
     
     if (!Directory.Exists(syncFolder))
         Directory.CreateDirectory(syncFolder);
     
     var backupZipFile = Path.Combine(backupFolder, $"{DateTime.UtcNow:yyyy.MM.dd.HH.mm.ss}.zip");
             
-    Console.Write($"Backup local files to: {backupFolder} ... ");
+    Logger.Info($"Backup local files to: {backupFolder} ... ");
     if (!Directory.Exists(backupFolder))
         Directory.CreateDirectory(backupFolder);
     
     ZipFile.CreateFromDirectory(setting.Path, backupZipFile);
-    Console.WriteLine($"finished.");
+    Logger.Info("finished.");
             
-    Console.Write($"Compare and sync files to local: .{Path.DirectorySeparatorChar}{syncFolder} ... ");
+    Logger.Info($"Compare and sync files to local: .{Path.DirectorySeparatorChar}{syncFolder} ... ");
             
     var syncFiles = Directory
         .GetFiles(syncFolder, setting.Filter, SearchOption.AllDirectories)
@@ -115,9 +152,9 @@ void Sync(string syncFolder, string backupFolder, SyncFolderSetting setting)
     
     foreach (var syncFile in syncFiles)
     {
-        var destFile = Path.Combine(setting.Path, Path.GetFileName(syncFile));
+        var destFile = Path.Combine(setting.Path, syncFile.Remove(0, syncFolder.Length + 1));
         File.Copy(syncFile, destFile);
     }
             
-    Console.WriteLine($"finished.");
+    Logger.Info("finished.");
 }
--- SyncFolderSettings.cs
+++ SyncFolderSettings.cs
@@ -9,5 +9,6 @@ public class SyncFolderSetting
 {
     public string Path { get; set; } = string.Empty;
     public string Process { get; set; } = string.Empty;
+    public byte Backups { get; set; } = 3;
     public string Filter { get; set; } = "*.*";
 }
